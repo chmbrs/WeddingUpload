34179141c0724ca6fd0db0aaab7cfd968d9fa31a
Chmbrs <juanjosechambers@gmail.com>
2019-01-15 11:31:50 -0200

    Models Fix

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Due to circular dependency
+# Same as ->
+# from upload.models import Photo
+# from django.apps import apps
+# Photo = apps.get_model('upload', 'Photo')
+
+class Like(models.Model):
+    user = models.ForeignKey(User, related_name='user_likes',
+                            on_delete=models.CASCADE, default=None)
+
+    # When you define a ForeignKey, you can call the model directly
+    photo = models.ForeignKey('upload.Photo', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ('user', 'photo')
--- upload/admin.py
+++ upload/admin.py
@@ -5,5 +5,5 @@ from .models import Photo
 # Register your models here.
 @admin.register(Photo)
 class DocumentAdmin(admin.ModelAdmin):
-    list_display = ("upload", "uploaded_at", "get_likes", "get_num_of_likes", "publish" )
+    list_display = ("upload", "uploaded_at", "get_num_of_likes", "publish" )
     list_editable = ["publish"]
